45a113be1173f30f9dd3b1071c5ef8c6d504293f
alicemirigo92 <alicemirigo92@gmai.com>
2023-01-10 23:08:17 +0300

    Modify js

--- script.js
+++ script.js
@@ -16,37 +16,37 @@ const allSpeakers = [
   {
     name: 'Natalie',
     position: 'Managing Director at Agri Foils',
-    image: 'Images/Natelie.jpg',
+    image: './Images/Natelie.jpg',
     bio: 'She is the first female director of Agri Foils. She holds a masters in Tree Planting.',
   },
   {
     name: 'Ajay',
     position: 'Chief Technical Director at Agri Foils',
-    image: 'Images/Ajay.jpg',
+    image: './Images/Ajay.jpg',
     bio: 'He pioneered the use of Botan524 to fight moths in wheat. He studied at the Universityu of Birmingham.',
   },
   {
     name: 'Malik',
     position: 'Accountant at Agri Foils',
-    image: 'Images/Malik.jpg',
+    image: './Images/Malik.jpg',
     bio: 'He is responsible for the smooth running of operations at Agri foils. His thesis on financial implications of Climate chenge bore him the 2012 Agri Expo award.',
   },
   {
     name: 'Sofia',
     position: 'Assistant Technical Director at Agri Foils',
-    image: 'Images/Sofia.jpg',
+    image: './Images/Sofia.jpg',
     bio: 'She studied at Texas University College, obtaining a degree in Agrinomics. She owns a hacienda where she farms GMO plants.',
   },
   {
     name: 'Surendra',
     position: 'Managing Director at Agri Bots',
-    image: 'Images/suredra.jpg',
+    image: './Images/suredra.jpg',
     bio: 'He created the Gant250 which is a machine used to harvest pineapples. He is regarded as a great inventor.',
   },
   {
     name: 'Mariatu',
     position: 'Chief Technical Director at Agri Bots',
-    image: 'Images/Mariatu.jpg',
+    image: './Images/Mariatu.jpg',
     bio: 'She has a PhD in Agriculture from Punjab University of Agriculture and Technology. Her publication on negative effects of GMOs was disruptive.',
   },
 ];
